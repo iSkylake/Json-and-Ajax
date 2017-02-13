7fad32d2338b423b34ff70410beffa4d29e00fb9
Llorch <SkylakeBeta@gmail.com>
2017-02-13 00:07:22 -0800

    Add dynamic json page load and hide button

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var btn = document.getElementById('btn');
 var animalContainer = document.getElementById('animal-info');
+var pageCounter = 1;
 
 btn.addEventListener('click', function(){
 	var ourRequest = new XMLHttpRequest();
-	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
 
 	ourRequest.onload = function(){
 		var data = JSON.parse(ourRequest.responseText);
 		renderHTML(data);
 	};
 	ourRequest.send();
+	pageCounter++;
+	if(pageCounter > 3){
+		btn.classList.add('hide-me');
+	}
 });
 
 function renderHTML(data){
