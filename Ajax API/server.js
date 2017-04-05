3e3518aad5299e00a136a749a274230432b4f369
Llorch <SkylakeBeta@gmail.com>
2017-04-04 22:29:25 -0700

    Install bodyparser and setup view

--- Ajax API/server.js
+++ Ajax API/server.js
@@ -1,9 +1,22 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3000;
 
+var todos = [
+	{
+		id: 1,
+		name: 'Eat food'
+	},
+	{
+		id: 2,
+		name: 'Take a bath'
+	}
+];
+
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 app.listen(PORT, function(){
 	console.log("Server listening on PORT: " + PORT);
